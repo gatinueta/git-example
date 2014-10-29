90f32ee373985f4595f823724384bb8e262b9655
Frank Ammeter <frank.ammeter@bsiag.com>
2014-10-29 21:29:51 +0100

    inside polygon corrected

--- python/tkinter.py
+++ python/tkinter.py
@@ -3,7 +3,18 @@ from random import randint,uniform
 import math
 from copy import deepcopy
 
-def point_inside_polygon(p,poly):
+def point_inside_polygon(p, poly):
+    i=0
+    j= len(poly)-1
+    c = False
+    while i < len(poly):
+        if ((poly[i].y > p.y) != (poly[j].y > p.y)) and (p.x < (poly[j].x-poly[i].x) * (p.y-poly[i].y) / (poly[j].y-poly[i].y) + poly[i].x) :
+            c = not c
+        j = i
+        i += 1
+    return c
+
+def point_inside_polygon2(p,poly):
     n = len(poly)
     inters=0
     p1 = poly[0]
@@ -35,8 +46,10 @@ class Point:
         return "[ " + str(self.x) + ", " + str(self.y) + " ]" 
 
 class tkanim:
-    def start_anim(self):
+    def draw_polygon(self, event):
         self.w.create_polygon([ ( p.x, p.y ) for p in self.w.bounds ], fill='', outline='green')
+
+    def start_anim(self):
         self.master.after(100, self.animate)
 
     def set_start(self, p):
@@ -44,15 +57,12 @@ class tkanim:
 
     def shoot(self,event):
         widget = event.widget
-        x = randint(0, widget.winfo_width()-10)
-        y = randint(0, widget.winfo_height()-10)
-        p = Point(x, y)
+        p = Point(event.x, event.y)
         if point_inside_polygon(p, widget.bounds):
             color = 'blue'
         else:
             color = 'red'
-        widget.create_oval(x-2, y-2, x+2, y+2, fill=color, outline=color)
-        self.master.after(100, self.shoot)
+        widget.create_oval(event.x-2, event.y-2, event.x+2, event.y+2, fill=color, outline=color)
  
     def __init__(self):
         self.master = Tk()
@@ -63,6 +73,7 @@ class tkanim:
         self.w.bind("<Button-1>", addpoint_callback)
         self.w.bind("<Button-2>", startanim_callback)
         self.w.bind("<Control-Button-1>", self.shoot)
+        self.w.bind("<Control-Button-2>", self.draw_polygon)
         self.w.pack()
         self.w.bounds = [] 
         self.SIZE = 2
@@ -81,11 +92,11 @@ class tkanim:
             self.p.y += uniform(0,2)*fy
             self.w.create_oval(self.p.x, self.p.y, self.p.x+self.SIZE, self.p.y+self.SIZE, fill="red", outline="red")
             steps += 1
-        self.w.create_oval(self.p.x, self.p.y, self.p.x+5*self.SIZE, self.p.y+5*self.SIZE, fill="blue", outline="blue")
+        #self.w.create_oval(self.p.x, self.p.y, self.p.x+5*self.SIZE, self.p.y+5*self.SIZE, fill="blue", outline="blue")
         self.direction = self.direction + math.pi + uniform(-.5,+.5)
         if self.direction > 2*math.pi:
             self.direction -= 2*math.pi
-        print "changing direction after ", steps, " steps"
+        #print "changing direction after ", steps, " steps"
         self.p = deepcopy(self.lastp)
         self.master.after(100, self.animate)
 
